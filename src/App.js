2c2a058426dee2a4bb30efec9f7d8f656d4c14b1
TheGuyNextToYou <thegutnexttoyou7@gmail.com>
2020-08-24 04:30:12 +0530

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './Components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes ={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
