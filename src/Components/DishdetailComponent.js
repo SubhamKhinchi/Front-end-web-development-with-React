1e244702de1b578f23f43c673378886fe5626376
TheGuyNextToYou <thegutnexttoyou7@gmail.com>
2020-09-29 11:43:19 +0530

    Single Page application part 2

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -1,61 +1,69 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
-    function RenderComment({comments})
+    function RenderComments({comments})
     {   
         if (comments!=null){
-            const cmnt = comments.map(com=>{
-                return(
-                    <React.Fragment>
-                    <li>{com.comment}</li><br />
-                    <li>-- {com.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(com.date)))}</li><br />
-                    </React.Fragment>
-                )
-
-            });
-            return(
-                <ul className="list-unstyled">{cmnt}</ul>
-            )
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment)=>{
+                            return(
+                                <li key={comment.key}>
+                                    <p>{comment.comment}</p>
+                                    <p>--{comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>
+                                </li>
+                            );
+                        })}
+    
+                    </ul>
+                </div>
+            );
         }
         else{
             return(<div></div>)
         }
     }
     function RenderDish({dish}){
-        if (dish != null)
-            return(
-                <>
-                    <div class="container">
-                        <div class="row">
-                            <div  className="col-12 col-md-5 m-1">
-                                <Card>
-                                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                                    <CardBody>
-                                        <CardTitle>{dish.name}</CardTitle>
-                                        <CardText>{dish.description}</CardText>
-                                    </CardBody>
-                                </Card>
-                            </div>
-                            <div  className="col-12 col-md-5 m-1">
-                                <h4>Comments</h4>
-                                <RenderComment comments= {dish.comments} />
-                            </div>
+        return(
+            <div  className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+    const Dishdetail =(props)=>{
+        if(props.dish !=null){
+            return (
+                <div className ="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu/'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}
+                            </h3>
+                            <hr />
                         </div>
+                        <RenderDish dish = {props.dish} />
+                        <RenderComments comments={props.comments} />
                     </div>
-                </>
+                </div>
             );
-        else
+        }
+        else{
             return(
                 <div></div>
             );
-    }
-    const Dishdetail =(props)=>{
-        return (
-            <div className="row">
-                <RenderDish dish = {props.dish} />
-            </div>
-        );
+        }
     }
   
   
